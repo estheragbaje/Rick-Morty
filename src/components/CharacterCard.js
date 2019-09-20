9b7f2bf6f663f865efbacfb725f994d0396907c1
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-09-20 09:03:21 +0100

    Installed styled components

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,19 @@
 import React from "react";
-import Text from "./Text"
+import Text from "./Text";
+import Image from "./Image";
 
 export default function CharacterCard(props) {
-  const { name, gender, image, status, species } = props.character;
+  const { name, location, image, status, species, origin } = props.character;
   return (
     <div>
-      <img src={src} />
-      <
+      <Image src={image} />
+      <Text>{name}</Text>
+      <Text>
+        <span>{species}</span>
+        <span>{status}</span>
+      </Text>
+      <Text>Location: {location.name}</Text>
+      <Text>Origin: {origin.name}</Text>
     </div>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -6,29 +6,27 @@ export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacters] = useState([]);
 
+  const getCharacter = () => {
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(response => {
+        console.log(response.data);
+        setCharacters(response.data.results);
+      })
+      .catch(error => {
+        console.error("Server Error", error);
+      });
+  };
+
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    const getCharacter = () => {
-      axios
-        .get("https://rickandmortyapi.com/api/character/")
-        .then(response => {
-          console.log(response.data);
-          setCharacters(response.data);
-        })
-        .catch(error => {
-          console.error("Server Error", error);
-        });
-    };
+    getCharacter();
   }, []);
 
   return (
     <section className="character-list">
-      <h2>
-        {characters.map(character => (
-          <CharacterCard key={character.id} character={character} />
-        ))}
-      </h2>
+      {characters.map(character => (
+        <CharacterCard key={character.id} character={character} />
+      ))}
     </section>
   );
 }

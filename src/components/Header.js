cc995111d69278b462e2e55e62a905cc7dec551f
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-09-20 10:02:17 +0100

    Styled image on welcome page using styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
+  const styling = { textDecoration: "none", fontSize: "18px", color: "black" };
+  const activeStyling = { color: "red", fontWeight: "bold" };
+
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
-      <Link to="/">Welcome</Link>
-      <Link to="/characters">Characters</Link>
+      <nav
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly"
+        }}
+      >
+        <NavLink style={styling} activeStyleing={activeStyling} to="/">
+          Home page
+        </NavLink>
+        <NavLink
+          style={styling}
+          activeStyleing={activeStyling}
+          to="/characters"
+        >
+          Characters
+        </NavLink>
+      </nav>
     </header>
   );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,11 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledImage = styled.img`
+  display: block;
+  width: 35%;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 1000px;
+`;
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
+        <StyledImage
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"

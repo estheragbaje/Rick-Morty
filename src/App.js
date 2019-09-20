2aec52ab3fc0da85080b647bad28c81defd15e1b
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-09-20 09:32:05 +0100

    Hooked up home & character page with react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage.js";
 
 export default function App() {
   return (
     <main>
       <Router>
         <Header />
-        <CharacterList />
+        <Route exact path="/" component={WelcomePage} />
+        <Route path="/characters" component={CharacterList} />
       </Router>
     </main>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <Link to="/">Welcome</Link>
+      <Link to="/characters">Characters</Link>
     </header>
   );
 }

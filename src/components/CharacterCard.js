bbd0dbb6383e1bbc10e9d3b48fb2c4712eccfc2b
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-09-20 08:39:45 +0100

    Created image component

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Text from "./Text"
 
 export default function CharacterCard(props) {
   const { name, gender, image, status, species } = props.character;
   return (
     <div>
       <img src={src} />
+      <
     </div>
   );
 }
